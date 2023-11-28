132fb53642f5e159bc1a35e884ab5a7af6c11157
Vladimir <V.Arutyunov@russianpost.ru>
2023-11-28 22:45:42 +0400

    feat(Box): Добавлено свойство boxShadow

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -8,16 +8,20 @@ const formatCssSizeValue = value => {
     return value;
 };
 
-const BoxWrapper = styled.div(({theme, width, height, ...props}) => {
+const BoxWrapper = styled.div(({theme, width, height, boxShadow}) => {
 
    return css`
         background: ${theme.ui.wrapper.bg};
         width: ${formatCssSizeValue(width)};
         height: ${formatCssSizeValue(height)};
-   `
-})
 
-const Box = forwardRef(({ width, height, children, as, ...props }, ref) => {
+        ${boxShadow && theme.ui.shadows[boxShadow]
+           ? `box-shadow: ${theme.ui.shadows[boxShadow]};`
+           : 'box-shadow: none;'
+    }
+`})
+
+const Box = forwardRef(({ width, height, children, as, boxShadow, ...BoxProps }, ref) => {
     const validElements = ['div', 'section', 'aside'];
     const wrapper = as && validElements.includes(as) ? as : 'div';
 
@@ -26,8 +30,9 @@ const Box = forwardRef(({ width, height, children, as, ...props }, ref) => {
         ref={ref} 
         width={width} 
         height={height}
+        boxShadow={boxShadow}
         as={wrapper}
-        {...props}
+        {...BoxProps}
     >
         {children}
     </BoxWrapper>
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -6,6 +6,10 @@ export const components = {
       wrapper: {
         bg: palettes.color[1],
       },
+      shadows: {
+        level1: "0px 4px 8px red", 
+        level2: "0px 6px 12px rgba(0, 0, 0, 0.15)", 
+      },
     },
   },
 
@@ -14,6 +18,10 @@ export const components = {
       wrapper: {
         bg: palettes.color[3]
       },
+      shadows: {
+        level1: "0px 4px 8px rgba(0, 0, 0, 0.12)",
+        level2: "0px 6px 12px rgba(0, 0, 0, 0.15)",
+      }
     },
   },
 };
